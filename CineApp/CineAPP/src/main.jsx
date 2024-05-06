ba470724ab27f82434171a76d54c86525b19be33
Logon Aluno <logonrmlocal@fiap.com.br>
2024-05-06 12:58:27 -0300

    React CineApp

---
+++ CineApp/CineAPP/src/main.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import Header from './Componentes/Header/Header.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Header></Header>
+  </React.StrictMode>,
+)
